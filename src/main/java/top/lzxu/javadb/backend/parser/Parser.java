f6916320be4e6af6797a7e56cdf7f2257b2c1fc1
swdyqw <155432770+swdyqw@users.noreply.github.com>
2024-07-09 12:50:10 +0800

    Update Parser.java

--- src/main/java/top/lzxu/javadb/backend/parser/Parser.java
+++ src/main/java/top/lzxu/javadb/backend/parser/Parser.java
@@ -1,450 +1,57 @@
-package top.lzxu.javadb.backend.parser;
+package top.lzxu.javadb.backend.utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
-import top.lzxu.javadb.backend.parser.statement.Abort;
-import top.lzxu.javadb.backend.parser.statement.Begin;
-import top.lzxu.javadb.backend.parser.statement.Commit;
-import top.lzxu.javadb.backend.parser.statement.Create;
-import top.lzxu.javadb.backend.parser.statement.Delete;
-import top.lzxu.javadb.backend.parser.statement.Drop;
-import top.lzxu.javadb.backend.parser.statement.Insert;
-import top.lzxu.javadb.backend.parser.statement.Select;
-import top.lzxu.javadb.backend.parser.statement.Show;
-import top.lzxu.javadb.backend.parser.statement.SingleExpression;
-import top.lzxu.javadb.backend.parser.statement.Update;
-import top.lzxu.javadb.backend.parser.statement.Where;
-import top.lzxu.javadb.common.Error;
+import com.google.common.primitives.Bytes;
 
 public class Parser {
-    public static Object Parse(byte[] statement) throws Exception {
-        top.lzxu.javadb.backend.parser.Tokenizer tokenizer = new top.lzxu.javadb.backend.parser.Tokenizer(statement);
-        String token = tokenizer.peek();
-        tokenizer.pop();
 
-        Object stat = null;
-        Exception statErr = null;
-        try {
-            switch(token) {
-                case "begin":
-                    stat = parseBegin(tokenizer);
-                    break;
-                case "commit":
-                    stat = parseCommit(tokenizer);
-                    break;
-                case "abort":
-                    stat = parseAbort(tokenizer);
-                    break;
-                case "create":
-                    stat = parseCreate(tokenizer);
-                    break;
-                case "drop":
-                    stat = parseDrop(tokenizer);
-                    break;
-                case "select":
-                    stat = parseSelect(tokenizer);
-                    break;
-                case "insert":
-                    stat = parseInsert(tokenizer);
-                    break;
-                case "delete":
-                    stat = parseDelete(tokenizer);
-                    break;
-                case "update":
-                    stat = parseUpdate(tokenizer);
-                    break;
-                case "show":
-                    stat = parseShow(tokenizer);
-                    break;
-                default:
-                    throw Error.InvalidCommandException;
-            }
-        } catch(Exception e) {
-            statErr = e;
-        }
-        try {
-            String next = tokenizer.peek();
-            if(!"".equals(next)) {
-                byte[] errStat = tokenizer.errStat();
-                statErr = new RuntimeException("Invalid statement: " + new String(errStat));
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-            byte[] errStat = tokenizer.errStat();
-            statErr = new RuntimeException("Invalid statement: " + new String(errStat));
-        }
-        if(statErr != null) {
-            throw statErr;
-        }
-        return stat;
+    public static byte[] short2Byte(short value) {
+        return ByteBuffer.allocate(Short.SIZE / Byte.SIZE).putShort(value).array();
     }
 
-    private static Show parseShow(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        String tmp = tokenizer.peek();
-        if("".equals(tmp)) {
-            return new Show();
-        }
-        throw Error.InvalidCommandException;
+    public static short parseShort(byte[] buf) {
+        ByteBuffer buffer = ByteBuffer.wrap(buf, 0, 2);
+        return buffer.getShort();
     }
 
-    private static Update parseUpdate(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        Update update = new Update();
-        update.tableName = tokenizer.peek();
-        tokenizer.pop();
-
-        if(!"set".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        update.fieldName = tokenizer.peek();
-        tokenizer.pop();
-
-        if(!"=".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        update.value = tokenizer.peek();
-        tokenizer.pop();
-
-        String tmp = tokenizer.peek();
-        if("".equals(tmp)) {
-            update.where = null;
-            return update;
-        }
-
-        update.where = parseWhere(tokenizer);
-        return update;
+    public static byte[] int2Byte(int value) {
+        return ByteBuffer.allocate(Integer.SIZE / Byte.SIZE).putInt(value).array();
     }
 
-    private static Delete parseDelete(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        Delete delete = new Delete();
-
-        if(!"from".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        String tableName = tokenizer.peek();
-        if(!isName(tableName)) {
-            throw Error.InvalidCommandException;
-        }
-        delete.tableName = tableName;
-        tokenizer.pop();
-
-        delete.where = parseWhere(tokenizer);
-        return delete;
+    public static int parseInt(byte[] buf) {
+        ByteBuffer buffer = ByteBuffer.wrap(buf, 0, 4);
+        return buffer.getInt();
     }
 
-    private static Insert parseInsert(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        Insert insert = new Insert();
-
-        if(!"into".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        String tableName = tokenizer.peek();
-        if(!isName(tableName)) {
-            throw Error.InvalidCommandException;
-        }
-        insert.tableName = tableName;
-        tokenizer.pop();
-
-        if(!"values".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-
-        List<String> values = new ArrayList<>();
-        while(true) {
-            tokenizer.pop();
-            String value = tokenizer.peek();
-            if("".equals(value)) {
-                break;
-            } else {
-                values.add(value);
-            }
-        }
-        insert.values = values.toArray(new String[values.size()]);
-
-        return insert;
+    public static long parseLong(byte[] buf) {
+        ByteBuffer buffer = ByteBuffer.wrap(buf, 0, 8);
+        return buffer.getLong();
     }
 
-    private static Select parseSelect(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        Select read = new Select();
-
-        List<String> fields = new ArrayList<>();
-        String asterisk = tokenizer.peek();
-        if("*".equals(asterisk)) {
-            fields.add(asterisk);
-            tokenizer.pop();
-        } else {
-            while(true) {
-                String field = tokenizer.peek();
-                if(!isName(field)) {
-                    throw Error.InvalidCommandException;
-                }
-                fields.add(field);
-                tokenizer.pop();
-                if(",".equals(tokenizer.peek())) {
-                    tokenizer.pop();
-                } else {
-                    break;
-                }
-            }
-        }
-        read.fields = fields.toArray(new String[fields.size()]);
-
-        if(!"from".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        String tableName = tokenizer.peek();
-        if(!isName(tableName)) {
-            throw Error.InvalidCommandException;
-        }
-        read.tableName = tableName;
-        tokenizer.pop();
-
-        String tmp = tokenizer.peek();
-        if("".equals(tmp)) {
-            read.where = null;
-            return read;
-        }
-
-        read.where = parseWhere(tokenizer);
-        return read;
+    public static byte[] long2Byte(long value) {
+        return ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(value).array();
     }
 
-    private static Where parseWhere(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        Where where = new Where();
-
-        if(!"where".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        SingleExpression exp1 = parseSingleExp(tokenizer);
-        where.singleExp1 = exp1;
-
-        String logicOp = tokenizer.peek();
-        if("".equals(logicOp)) {
-            where.logicOp = logicOp;
-            return where;
-        }
-        if(!isLogicOp(logicOp)) {
-            throw Error.InvalidCommandException;
-        }
-        where.logicOp = logicOp;
-        tokenizer.pop();
-
-        SingleExpression exp2 = parseSingleExp(tokenizer);
-        where.singleExp2 = exp2;
-
-        if(!"".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        return where;
+    public static ParseStringRes parseString(byte[] raw) {
+        int length = parseInt(Arrays.copyOf(raw, 4));
+        String str = new String(Arrays.copyOfRange(raw, 4, 4+length));
+        return new ParseStringRes(str, length+4);
     }
 
-    private static SingleExpression parseSingleExp(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        SingleExpression exp = new SingleExpression();
-        
-        String field = tokenizer.peek();
-        if(!isName(field)) {
-            throw Error.InvalidCommandException;
-        }
-        exp.field = field;
-        tokenizer.pop();
-
-        String op = tokenizer.peek();
-        if(!isCmpOp(op)) {
-            throw Error.InvalidCommandException;
-        }
-        exp.compareOp = op;
-        tokenizer.pop();
-
-        exp.value = tokenizer.peek();
-        tokenizer.pop();
-        return exp;
+    public static byte[] string2Byte(String str) {
+        byte[] l = int2Byte(str.length());
+        return Bytes.concat(l, str.getBytes());
     }
 
-    private static boolean isCmpOp(String op) {
-        return ("=".equals(op) || ">".equals(op) || "<".equals(op));
+    public static long str2Uid(String key) {
+        long seed = 13331;
+        long res = 0;
+        for(byte b : key.getBytes()) {
+            res = res * seed + (long)b;
+        }
+        return res;
     }
 
-    private static boolean isLogicOp(String op) {
-        return ("and".equals(op) || "or".equals(op));
-    }
-
-    private static Drop parseDrop(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        if(!"table".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        String tableName = tokenizer.peek();
-        if(!isName(tableName)) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        if(!"".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        
-        Drop drop = new Drop();
-        drop.tableName = tableName;
-        return drop;
-    }
-
-    private static Create parseCreate(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        if(!"table".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        Create create = new Create();
-        String name = tokenizer.peek();
-        if(!isName(name)) {
-            throw Error.InvalidCommandException;
-        }
-        create.tableName = name;
-
-        List<String> fNames = new ArrayList<>();
-        List<String> fTypes = new ArrayList<>();
-        while(true) {
-            tokenizer.pop();
-            String field = tokenizer.peek();
-            if("(".equals(field)) {
-                break;
-            }
-
-            if(!isName(field)) {
-                throw Error.InvalidCommandException;
-            }
-
-            tokenizer.pop();
-            String fieldType = tokenizer.peek();
-            if(!isType(fieldType)) {
-                throw Error.InvalidCommandException;
-            }
-            fNames.add(field);
-            fTypes.add(fieldType);
-            tokenizer.pop();
-            
-            String next = tokenizer.peek();
-            if(",".equals(next)) {
-                continue;
-            } else if("".equals(next)) {
-                throw Error.TableNoIndexException;
-            } else if("(".equals(next)) {
-                break;
-            } else {
-                throw Error.InvalidCommandException;
-            }
-        }
-        create.fieldName = fNames.toArray(new String[fNames.size()]);
-        create.fieldType = fTypes.toArray(new String[fTypes.size()]);
-
-        tokenizer.pop();
-        if(!"index".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-
-        List<String> indexes = new ArrayList<>();
-        while(true) {
-            tokenizer.pop();
-            String field = tokenizer.peek();
-            if(")".equals(field)) {
-                break;
-            }
-            if(!isName(field)) {
-                throw Error.InvalidCommandException;
-            } else {
-                indexes.add(field);
-            }
-        }
-        create.index = indexes.toArray(new String[indexes.size()]);
-        tokenizer.pop();
-
-        if(!"".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        return create;
-    }
-
-    private static boolean isType(String tp) {
-        return ("int32".equals(tp) || "int64".equals(tp) ||
-        "string".equals(tp));
-    }
-
-    private static Abort parseAbort(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        if(!"".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        return new Abort();
-    }
-
-    private static Commit parseCommit(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        if(!"".equals(tokenizer.peek())) {
-            throw Error.InvalidCommandException;
-        }
-        return new Commit();
-    }
-
-    private static Begin parseBegin(top.lzxu.javadb.backend.parser.Tokenizer tokenizer) throws Exception {
-        String isolation = tokenizer.peek();
-        Begin begin = new Begin();
-        if("".equals(isolation)) {
-            return begin;
-        }
-        if(!"isolation".equals(isolation)) {
-            throw Error.InvalidCommandException;
-        }
-
-        tokenizer.pop();
-        String level = tokenizer.peek();
-        if(!"level".equals(level)) {
-            throw Error.InvalidCommandException;
-        }
-        tokenizer.pop();
-
-        String tmp1 = tokenizer.peek();
-        if("read".equals(tmp1)) {
-            tokenizer.pop();
-            String tmp2 = tokenizer.peek();
-            if("committed".equals(tmp2)) {
-                tokenizer.pop();
-                if(!"".equals(tokenizer.peek())) {
-                    throw Error.InvalidCommandException;
-                }
-                return begin;
-            } else {
-                throw Error.InvalidCommandException;
-            }
-        } else if("repeatable".equals(tmp1)) {
-            tokenizer.pop();
-            String tmp2 = tokenizer.peek();
-            if("read".equals(tmp2)) {
-                begin.isRepeatableRead = true;
-                tokenizer.pop();
-                if(!"".equals(tokenizer.peek())) {
-                    throw Error.InvalidCommandException;
-                }
-                return begin;
-            } else {
-                throw Error.InvalidCommandException;
-            }
-        } else {
-            throw Error.InvalidCommandException;
-        }
-    }
-
-    private static boolean isName(String name) {
-        return !(name.length() == 1 && !Tokenizer.isAlphaBeta(name.getBytes()[0]));
-    }
 }
